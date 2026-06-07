e405d8f125dccc40408dbc1b5c0d19361153e44d
Sophie LDL <sophievigneron33@gmail.com>
2026-06-07 13:30:25 +0200

    add components directory and add a title to main app with MyTitle component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import MyTitle from "./components/MyTitle.tsx"
 
 function App() {
   return (
     <section>
-      <h1>Hello React</h1>
+      <MyTitle />
     </section>
   );
 }
