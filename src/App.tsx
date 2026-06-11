81ff8cb98478f6a21674057360b2293f307a952c
Sophie LDL <sophievigneron33@gmail.com>
2026-06-11 12:01:07 +0200

    finished map() exercise

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,20 @@ const pokemonList = [
     name: "Bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  }, {
+    name: "Charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "Squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "Pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
     name: "Mew",
@@ -25,8 +39,11 @@ function App() {
 
   return (
     <div>
-      <button type="button" onClick={() => setPokemonName("Bulbasaur")}>Bulbi</button>
-      <button type="button" onClick={() => setPokemonName("Mew")}>Mew</button>
+      <nav>
+        {pokemonList.map((pokemon) => (
+          <button key={pokemon.name} onClick={() => setPokemonName(pokemon.name)}>{pokemon.name}</button>
+        ))}
+      </nav>
       <PokemonCard pokemon={pokemon} />
     </div>
   );
